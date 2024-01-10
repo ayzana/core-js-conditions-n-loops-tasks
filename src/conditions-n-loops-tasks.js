1b8e8c3440b382004bcd2a7d5fe0775f1087f4ab
ayzana <aizanka001@gmail.com>
2024-01-10 16:47:54 +0300

    feat: add functions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,11 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number >= 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -38,8 +41,14 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  }
+  if (b > a && b > c) {
+    return b;
+  }
+  return c;
 }
 
 /**
@@ -60,8 +69,16 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (
+    (queen.x === queen.y && king.x === king.y) ||
+    queen.x === king.x ||
+    queen.y === king.y ||
+    Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -82,8 +99,14 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  return (
+    !!(a === b || b === c || a === c) &&
+    a !== 0 &&
+    b !== 0 &&
+    c !== 0 &&
+    a + b > c
+  );
 }
 
 /**
@@ -100,8 +123,26 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  if (num < 1) {
+    return '';
+  }
+  if (num >= 10) {
+    return `X${convertToRomanNumerals(num - 10)}`;
+  }
+  if (num >= 9) {
+    return `IX${convertToRomanNumerals(num - 9)}`;
+  }
+  if (num >= 5) {
+    return `V${convertToRomanNumerals(num - 5)}`;
+  }
+  if (num >= 4) {
+    return `IV${convertToRomanNumerals(num - 4)}`;
+  }
+  if (num >= 1) {
+    return `I${convertToRomanNumerals(num - 1)}`;
+  }
+  return convertToRomanNumerals(num);
 }
 
 /**
@@ -119,10 +160,60 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let result = '';
+  const iterator = numberStr[Symbol.iterator]();
+  let theChar = iterator.next();
+  while (!theChar.done) {
+    switch (theChar.value) {
+      case '-':
+        result += `minus`;
+        break;
+      case '.':
+        result += `point`;
+        break;
+      case ',':
+        result += `point`;
+        break;
+      case '0':
+        result += `zero`;
+        break;
+      case '1':
+        result += `one`;
+        break;
+      case '2':
+        result += `two`;
+        break;
+      case '3':
+        result += `three`;
+        break;
+      case '4':
+        result += `four`;
+        break;
+      case '5':
+        result += `five`;
+        break;
+      case '6':
+        result += `six`;
+        break;
+      case '7':
+        result += `seven`;
+        break;
+      case '8':
+        result += `eight`;
+        break;
+      case '9':
+        result += `nine`;
+        break;
+      default:
+    }
+    theChar = iterator.next();
+    if (!theChar.done) {
+      result += ' ';
+    }
+  }
+  return result;
 }
-
 /**
  * Determines whether a string is a palindrome.
  * In this task, the use of methods of the String and Array classes is not allowed.
